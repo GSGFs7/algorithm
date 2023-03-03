2d0f1fb5bcf1d410d3fe779ad866976f7762e23f
GSGFs <gsgfs@gsgfs.moe>
2023-03-03 18:53:40 +0800

    位运算

---
+++ 1.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int lowBit(int x)
+{
+    return x & -x;
+    // & 位与运算 当两个值都为1时结果为1 0&0为0
+    // 可获得x二进制的最后一位
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    while (n--)
+    {
+        int x;
+        cin >> x;
+
+        int res = 0;
+        while (x) x -= lowBit(x), res++;
+        // 每次都减去最后一位
+
+        cout << res << ' ';
+    }
+    return 0;
+}
